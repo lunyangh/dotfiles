82097625427b715d3dfa1aeb41b80eaf5b062921
lunyangh <hly.nick19993@gmail.com>
2021-01-01 23:30:21 -0500

    update setup file

--- setup.sh
+++ setup.sh
@@ -1,4 +1,15 @@
+# vim configuration
 ln -sf "/Users/lunyanghuang/Dropbox (Personal)/Projects/Terminal/dotfiles/Vim config/.vimrc" ~/.vimrc
+# zsh configuration
 ln -sf "/Users/lunyanghuang/Dropbox (Personal)/Projects/Terminal/dotfiles/ZSH config/.zshrc" ~/.zshrc
+# zprezto configuration
 ln -sf "/Users/lunyanghuang/Dropbox (Personal)/Projects/Terminal/dotfiles/ZSH config/.zpreztorc" ~/.zpreztorc
 ln -sf "/Users/lunyanghuang/Dropbox (Personal)/Projects/Terminal/dotfiles/ZSH config/.gvimrc" ~/.gvimrc
+
+# tmux code
+ln -sf "/Users/lunyanghuang/Dropbox (Personal)/Projects/Terminal/dotfiles/tmux config/.tmux.conf"  ~/.tmux.conf
+
+ln -sf "/Users/lunyanghuang/Dropbox (Personal)/Projects/Terminal/dotfiles/tmux config/.tmux_status_bar"  ~/.tmux_status_bar
+
+# python snippets
+ln -sf "/Users/lunyanghuang/Dropbox (Personal)/Projects/Terminal/dotfiles/Vim config/snippets/python.snippets" ~/.vim/user_snippets/python.snippets
