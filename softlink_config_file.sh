68646fe05177928fdafe82bf764dac8758690015
Lunyang Huang <lunyanghuang@Lunyangs-MacBook-Pro.local>
2025-07-13 22:20:25 -0400

    vscode: * fix shortcut * adding snippet as softlink to dotfile.

--- softlink_config_file.sh
+++ softlink_config_file.sh
@@ -1,3 +1,23 @@
+#!/bin/bash
+
+# Exit upon first error
+set -e
+# change directory to bash script
+pushd "$(dirname "")" >/dev/null
+# print error upon exit,  return to original dir regardless error or not
+handle_error() {
+  echo "Error: Command failed at line BASH_LINENO" >&2
+  popd >/dev/null 
+  exit 1
+}
+trap 'handle_error' ERR
+trap 'popd >/dev/null 2>&1' EXIT
+
+# --- script content goes below this line ---
+
+echo "Starting script in directory: $(pwd)"
+echo "Script finished successfully."
+
 # path variable 
 dotfile_path="$HOME/dropbox/project/terminal/dotfiles"
 vs_code_config_path="$HOME/Library/Application Support/Code/User"
@@ -24,6 +44,13 @@ ln -sf "$dotfile_path/ZSH config/.jupytext" ~/.config/.jupytext
 # vs code configuration
 ln -sf "$dotfile_path/vs_code_config/settings.json" "$vs_code_config_path/settings.json"
 ln -sf "$dotfile_path/vs_code_config/keybindings.json" "$vs_code_config_path/keybindings.json"
+
+# snippet files 
+ln -sf "$dotfile_path/vs_code_config/snippet/shell_snippet.json" "$vs_code_config_path/snippets/shellscript.json"
+
+ln -sf "$dotfile_path/vs_code_config/snippet/python_snippet.json" "$vs_code_config_path/snippets/python.json"
+
+
 
 # ssh configuration 
 ln -sf "$dotfile_path/ssh_config/ssh_config" "$ssh_config_path/config"
